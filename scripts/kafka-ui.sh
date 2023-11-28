011e2e75927ef7d01c9c38beabe00cd50b13b791
Amir Bashiri <amir.bashiri@zoodfood.com>
2023-11-28 17:23:08 +0330

    Add kafka-ui user

--- scripts/kafka-ui.sh
+++ scripts/kafka-ui.sh
@@ -20,6 +20,8 @@ sudo chown 100:101 /opt/kafka-ui/data/ -R
     echo "KAFKA_ui_PASSWORD=${KAFKA_ui_PASSWORD}"
 } | sudo tee /opt/kafka-ui/.env
 
+/opt/kafka/bin/kafka-acls.sh --bootstrap-server localhost:9092 --command-config /opt/kafka/config/admin.conf --add --allow-principal User:user --consumer --topic '*' --group user
+
 ## Load docker image from cache
 [ -f $INSTALATION_PATH/cache/kafka-ui.tgz ] && sudo docker load --input $INSTALATION_PATH/cache/kafka-ui.tgz || true
 
